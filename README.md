ed28c0cf6b59dc55c9e7d75bd42760002500ff91
Fabio Ferreira <fabiomf@protonmail.com>
2019-11-08 13:41:43 -0400

    Testa banco de dados

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Server
 Na pasta `server`
+
 Para instalar as dependências `npm i`
+
 Para transpilar o Typescript `tsc -w`
+
 Para servir a api `nodemon dist/index.js`
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,35 @@
 import * as express from 'express';
+import { Sequelize, Model, DataTypes, BuildOptions } from 'sequelize';
 
 const app = express();
 const port = 3000;
+
 app.get('/', (req, res, next) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => console.log(`Escutando na porta ${port}`))
+
+const sequelize = new Sequelize('postgres://postgres:postgres@localhost:5432/estacionamento');
+
+class Entrada extends Model {
+  public id!: number; 
+  public placa!: string;
+  public hora: string;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+Entrada.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  placa: new DataTypes.STRING(8),
+  hora: DataTypes.TIME
+}, {tableName: 'entradas', sequelize, timestamps: false})
+
+Entrada.findAll().then(c => console.log(c))
+
